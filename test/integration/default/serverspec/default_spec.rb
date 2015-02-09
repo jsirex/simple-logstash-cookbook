6166186a287cb00841d80c37a637e862e8c32991
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-02-09 15:26:44 +0300

    Added more specs

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -10,7 +10,22 @@ describe command('/opt/logstash/bin/logstash -t -f /etc/logstash') do
   its(:exit_status) { should eq 0 }
 end
 
+describe command('/opt/logstash/bin/logstash -t -f /etc/logstash-two') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe command('ls /opt/logstash/.sincedb_*') do
+  its(:exit_status) { should eq 0 }
+end
+
+%w(logstash logstash-two).each do |svc|
+  describe command("/etc/init.d/#{svc} status") do
+    its(:exit_status) { should eq 0 }
+  end
+end
+
 [
+  # Configs
   '/etc/logstash/10_input_test1.conf',
   '/etc/logstash/10_input_test2.conf',
   '/etc/logstash/20_filter_test1.conf',
@@ -18,9 +33,13 @@ end
   '/etc/logstash/90_output_test1.conf',
   '/etc/logstash-two/10_input_test3.conf',
   '/etc/logstash-two/20_filter_test3.conf',
-  '/etc/logstash-two/90_output_test3.conf'
+  '/etc/logstash-two/90_output_test3.conf',
+  # Produced files
+  '/tmp/result_test1.logstash',
+  '/tmp/result_test3.logstash'
 ].each do |f|
   describe file(f) do
     it { should be_file }
-  end  
+  end
 end
+
