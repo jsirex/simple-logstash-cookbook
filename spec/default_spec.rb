6ad630cfc41470ea297a88c8633b84c4e135ac8f
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-04-09 12:50:12 +0300

    Improve tests for #2

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
 describe 'simple-logstash::default' do
-  cached(:chef_run) { ChefSpec::ServerRunner.new.converge described_recipe }
+  cached(:chef_run) do
+    ChefSpec::ServerRunner.new do |node|
+      node.override['logstash']['user'] = 'lgu'
+      node.override['logstash']['group'] = 'lgg'
+    end.converge(described_recipe)
+  end
 
   it 'creates user for logstash' do
-    expect(chef_run).to create_user('logstash')
+    expect(chef_run).to create_user('lgu')
   end
 
   it 'creates group for logstash' do
-    expect(chef_run).to create_group('logstash')
+    expect(chef_run).to create_group('lgg')
   end
 
   # No matchers in 0.9.0 ark. Wait for new version
