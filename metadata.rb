28760353d93c357ecfd8522e226d2942f18bad9c
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-04-09 13:18:37 +0300

    Bump new development version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures simple-logstash. No less. No more.'
 long_description 'Installs/Configures simple-logstash. No less. No more.'
 issues_url       'https://github.com/jsirex/simple-logstash-cookbook/issues'
 source_url       'https://github.com/jsirex/simple-logstash-cookbook'
-version          '0.2.2'
+version          '0.2.3'
 
 supports 'debian'
 supports 'ubuntu'
