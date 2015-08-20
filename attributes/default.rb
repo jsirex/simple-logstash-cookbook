d97ccedc57dbf44426e9921c546a80647de41839
Yauhen Artsiukhou <yauhen_artsiukhou@epam.com>
2015-08-20 22:25:43 +0300

    Update logstash to 1.5.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
-default['logstash']['download_url'] = 'https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz'
-default['logstash']['checksum'] = 'd5be171af8d4ca966a0c731fc34f5deeee9d7631319e3660d1df99e43c5f8069'
-default['logstash']['version'] = '1.4.2'
+default['logstash']['download_url'] = 'https://download.elastic.co/logstash/logstash/logstash-1.5.3.tar.gz'
+default['logstash']['checksum'] = 'eb3c366074e561d777348bfe9db3d4d1cccbf2fa8e7406776f500b4ca639c4aa'
+default['logstash']['version'] = '1.5.3'
 
 default['logstash']['user'] = 'logstash'
 default['logstash']['group'] = 'logstash'
