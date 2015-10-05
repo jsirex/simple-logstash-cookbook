d9b6a419d9e25bb174b384c7614b6996c10859da
Jesse DeRose <jesse@anyperk.com>
2015-10-05 09:31:43 -0700

    Update metadata.rb for Chef 11 compatibility.

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'jsirex@gmail.com'
 license          'Apache 2'
 description      'Installs/Configures simple-logstash. No less. No more.'
 long_description 'Installs/Configures simple-logstash. No less. No more.'
-issues_url       'https://github.com/jsirex/simple-logstash-cookbook/issues'
-source_url       'https://github.com/jsirex/simple-logstash-cookbook'
+issues_url       'https://github.com/jsirex/simple-logstash-cookbook/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/jsirex/simple-logstash-cookbook' if respond_to?(:source_url)
 version          '0.2.4'
 
 supports 'debian'
